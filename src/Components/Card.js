e70080d218d636a97ae0b9ada3d36f7549c22760
kushalrj <kusharma759@gmail.com>
2024-05-24 13:05:48 +0530

    d

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,23 +1,29 @@
 import React from 'react'
 
 export default function Card(props) {
+
+    let options= props.options[0];
+    let priceOptions= Object.keys(options);
     return (
         <div>  <div className="card mt-3 " style={{ "width": "18rem" }}>
-            <img className="card-img-top" src="https://www.sainsburysmagazine.co.uk/uploads/media/720x770/06/7696-chilli_paneer_skewers.jpg?v=1-0" alt="Card image cap" />
+            <img className="card-img-top" src={props.img} alt="Card image cap" />
             <div className="card-body">
                 <h5 className="card-title">{props.name}</h5>
-                <p className="card-text">Some quick example text to buildthe card's content.</p>
+            
                 <div className='container w-100'>
-                    <select className='m-2 h-100 bg-sucess'>
+                    <select className='m-2 h-100 bg-success'>
                         {Array.from(Array(6), (e, i) => {
                             return (
                                 <option key={i + 1} value={i + 1}>{i + 1}  </option>
                             )
                         })}
                     </select>
-                    <select className='m-2 h-100 bg-sucess rounded'>
-                        <option value="half">Half</option>
-                        <option value="Full">Full</option>
+                    <select className='m-2 h-100 bg-success rounded'>
+                      {priceOptions.map((data)=>
+                      {
+                        return <option key={data} value={data}>{data}</option>
+                      }
+                    )}
                     </select>
                     <div className='d-inline h-100 fs-5'>
                         Total Price
--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -8,6 +8,7 @@ export default function Home() {
 
   const [foodCat, setFoodCat] = useState([]);
   const [foodItem, setFoodItem] = useState([]);
+  const [search, setSearch] = useState('');
 
   const loadData = async () => {
     let response = await fetch("http://localhost:5000/api/foodData", {
@@ -34,7 +35,35 @@ export default function Home() {
     <div>
 
       <div> <Navbar /> </div>
-      <Crousel />
+        {/* <Crousel /> */}
+       <div> <div id="carouselExampleFade" className="carousel slide carousel-fade " data-bs-ride="carousel">
+
+                <div className="carousel-inner " id='carousel'>
+                    <div className=" carousel-caption  " style={{ zIndex: "9" }}>
+                        <div className=" d-flex justify-content-center">  {/* justify-content-center, copy this <form> from navbar for search box */}
+                            <input className="form-control me-2 w-75 bg-white text-dark" type="search" placeholder="Search" aria-label="Search" value={search} onChange={(e)=>{setSearch(e.target.value)}} />
+                            {/* <button className="btn text-white bg-success" type="submit">Search</button> */}
+                        </div>
+                    </div>
+                    <div className="carousel-item active" >
+                        <img src="https://source.unsplash.com/random/900x700/?burger" className="d-block w-100  " style={{ filter: "brightness(30%)" }} alt="..." />
+                    </div>
+                    <div className="carousel-item">
+                        <img src="https://source.unsplash.com/random/900x700/?pastry" className="d-block w-100 " style={{ filter: "brightness(30%)" }} alt="..." />
+                    </div>
+                    <div className="carousel-item">
+                        <img src="https://source.unsplash.com/random/900x700/?barbeque" className="d-block w-100 " style={{ filter: "brightness(30%)" }} alt="..." />
+                    </div>
+                </div>
+                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Previous</span>
+                </button>
+                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Next</span>
+                </button>
+            </div></div>
       <div className='container'>
         {
           foodCat.length > 0
@@ -46,7 +75,7 @@ export default function Home() {
                   <hr />
                   {
                     // here on first iteration "Rice" cat will there then it filter out foodItem with rice categgory"
-                    foodItem.length > 0 ? foodItem.filter((item) => item.CategoryName === data.CategoryName)
+                    foodItem.length > 0 ? foodItem.filter((item) => item.CategoryName === data.CategoryName && item.name.toLowerCase().includes(search.toLocaleLowerCase()))
                       .map(filterItems => {
                         return(
                           <div key={filterItems._id} className="col-12 col-md-6 col-lg-3">
